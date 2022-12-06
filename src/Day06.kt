5836682fe0cd88b4feb9091d416af183f7f70317
Manel Martos <manuel.martos-roldan@team.bumble.com>
2022-12-06 06:41:24 +0100

    Prepare day 6

---
+++ src/Day06.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("part01 -> ${solveDay06Part01()}")
+    println("part02 -> ${solveDay06Part02()}")
+}
+
+private fun solveDay06Part01() = Unit
+
+private fun solveDay06Part02() = Unit
